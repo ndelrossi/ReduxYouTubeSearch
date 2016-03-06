ad3cc761b888cf6d787aeb8755a2e265e28ad103
Nick DelRossi <nickwtf@gmail.com>
2016-03-06 16:37:06 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyAvK9IHzflW1GFlR3WWyOpfsz9jK52UH60';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      sellectedVideo: null
+    };
+
+    this.videoSearch('paul simon');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange={term => this.videoSearch({term})}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
